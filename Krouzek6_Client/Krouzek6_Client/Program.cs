8a79b15c6c00b7638fac4be14ccb4deebde0ee47
kvetinac97 <kvetinac97@email.cz>
2024-12-26 11:21:41 +0100

    Initial commit (2024)

---
+++ Krouzek6_Client/Krouzek6_Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Text.Json;
+
+class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Strength { get; set; }
+
+    public Character(string name, int health, int strength)
+    {
+        this.Name = name;
+        this.Health = health;
+        this.Strength = strength;
+    }
+}
+
+class StudentClient
+{
+    static void Main(string[] args)
+    {
+        Character character = new Character("Golem", 500, 10);
+
+        // Připojení k serveru
+        string serverIp = "192.168.108.86"; // Místní server
+        int serverPort = 12345; // Port serveru
+
+        try
+        {
+            using (TcpClient client = new TcpClient(serverIp, serverPort))
+            using (NetworkStream stream = client.GetStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                // Serializace objektu postavy na JSON a odeslání na server
+                string json = JsonSerializer.Serialize(character);
+                writer.WriteLine(json);  // Odeslání JSON řetězce na server
+                writer.Flush();          // Ujistíme se, že všechna data byla odeslána
+                Console.WriteLine("Character sent to the server.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Kroužek programování 23/24
+
+Kódy z kroužku programování 2023/2024 v SVČ Amos.
+
+## První pololetí
+
+Textové programy -- seznámení se s proměnnými, vstupem, výstupem, větvení, cykly, funkcemi a objekty.
+
+Zajímavé programy: Aréna s bojovníky, Generátor příběhů, Akinátor, Bludiště.
+
